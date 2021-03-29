658e24b24eff3efd3e9952f14e5ac54842ed2d94
himzei <himzei@gmail.com>
2021-03-29 20:57:51 +0900

    15.0 Tab Navigator

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,13 +1,61 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Feed from "../screens/Feed";
+import Search from "../screens/Search";
+import Notifications from "../screens/Notifications";
+import Profile from "../screens/Profile";
 
 const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Feed" component={Feed} />
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: "white",
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "rgba(255, 255, 255, 0.2)",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="home" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="search" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+        name="Search"
+        component={Search}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="heart" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+        name="Notifications"
+        component={Notifications}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name="person" color={color} size={focused ? 24 : 20} />
+          ),
+        }}
+        name="Profile"
+        component={Profile}
+      />
     </Tabs.Navigator>
   );
 }
