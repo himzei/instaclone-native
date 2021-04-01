818098545d70a27c963496e1bbd7e85daec14816
himzei <himzei@gmail.com>
2021-04-01 19:42:24 +0900

    18.1 Rooms Screen part One

--- screens/Rooms.js
+++ screens/Rooms.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { Text, FlatList } from "react-native";
+import { ROOM_FRAGMENT } from "../fragments";
+import styled from "styled-components/native";
+import ScreenLayout from "../components/ScreenLayout";
+
+const SEE_ROOMS_QUERY = gql`
+  query seeRooms {
+    seeRooms {
+      ...RoomParts
+    }
+  }
+  ${ROOM_FRAGMENT}
+`;
+
+const RooomContainer = styled.View`
+  background-color: black;
+`;
+
+const RoomText = styled.Text`
+  color: white;
+`;
 
 export default function Rooms() {
+  const { data, loading } = useQuery(SEE_ROOMS_QUERY);
+  const renderItem = ({ item: room }) => (
+    <RooomContainer>
+      <RoomText>
+        {room.unreadTotal === "0"
+          ? "Name of the other person"
+          : `${room.unreadTotal} messages.`}
+      </RoomText>
+    </RooomContainer>
+  );
   return (
-    <View>
-      <Text>Rooms List</Text>
-    </View>
+    <ScreenLayout loading={loading}>
+      <FlatList
+        data={data?.seeRooms}
+        keyExtractor={(room) => "" + room.id}
+        renderItem={renderItem}
+      />
+    </ScreenLayout>
   );
 }
